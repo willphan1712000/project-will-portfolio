afa7842ea3e6cd2d393aa2579454364ccc201045
willphan <phanthanhnha1712000@gmail.com>
2025-01-21 16:46:03 -0500

    Add travel collection page

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -4,7 +4,9 @@ import { useEffect } from "react"
 import PictureSection from "../components/PictureSection"
 import SecondTitle from "../components/SecondTitle"
 import homepage from "../execute"
+import Card from "./_components/Card"
 import { pictureContent, pictures } from "./pictureContent"
+
 
 export default function Blog() {
   useEffect(() => {
@@ -14,10 +16,14 @@ export default function Blog() {
   return (
     <>
       <div className="flex justify-center items-center w-full">
-        <div id="main" className="md:!h-[800vh] max-w-[1500px]">
-            <SecondTitle content={{up: "My journey", down: "begins"}}/>
+        <div id="main" className="md:!h-[800vh] max-w-[1500px] w-[1500px]">
+            <SecondTitle content={{up: "My travel", down: "begins"}}/>
             <PictureSection pictureContent={pictureContent} pictures={pictures}/>
         </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <Card />
       </div>
     </>
   );
