f4aa272918b756ee76ef8a1ff494ccb7e1aee04a
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2025-11-30 22:01:20 -0500

    Change intro and image next to it

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -17,7 +17,7 @@ const Intro = () => {
     <>
       <div className='bg-gradient-to-r from-purple-500 via-red-500 to-yellow-500 m-[2rem] rounded-[0.7rem] p-[0.15rem]'>
         <div className='bg-white lg:flex-row flex-col' id="introduction">
-          <div className="img md:p-[1rem]"><Image draggable="false" src={'/assets/ksu.png'} alt="will_intro" width="300" height="200"/></div>
+          <div className="img md:p-[1rem]"><Image draggable="false" src={'/assets/self1.JPEG'} alt="will_intro" width="300" height="200"/></div>
           <div className="text">
               <div className="heading">{home.intro}</div>
               <div className="des">{home.des}</div>
--- app/static/home.ts
+++ app/static/home.ts
@@ -1,8 +1,7 @@
 export default Object.freeze({
     heading: "I am Will – a Full Stack Engineer, DevOps Engineer, and Machine Learning Enthusiast",
-    intro: "I expect to graduate from Kennesaw State University – Georgia, in May 2026, and I am continuously learning more and more knowledge to level up my skills and experience in the tech industry.",
-    des: "I have faced several challenges after moving to the US, which made me become the person that I am now. For that, I am very proud of it, and also because those struggles help get me back up and continue this exciting journey in the US. Let’s begin my share of journey while I was learning to become a Software Engineer.",
+    intro: "Opportunities are everywhere",
+    des: "Every moment always is a good chance for me to learn and gain experience in anything and everything. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others.",
     up: "My Full Stack Journey",
     down: "begins HERE",
-    
 })
