260ec81fc6632896b1b4e5932cbf034e3fc8096d
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-04-16 16:06:19 -0400

    Refine styling

--- app/components/Subbackground/Title/Title.tsx
+++ app/components/Subbackground/Title/Title.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import styles from './title.module.css'
-import home from '@/app/static/home'
-import Buttons from '@/app/components/Subbackground/Buttons/Buttons'
+import Buttons from '@/app/components/Subbackground/Buttons/Buttons';
+import config from '@/app/static/config';
+import home from '@/app/static/home';
+import { ModernButton } from '@willphan1712000/frontend';
+import { TypeAnimation } from 'react-type-animation';
+import styles from './title.module.css';
 
 const Title = () => {
   return (
     <div className={styles.title}>
-        <h1 className='text-[2rem] md:text-[3rem]'>My Portfolio</h1>
-        <h1 className='text-[1.5rem] md:text-[2rem]'>{home.heading}</h1>
+        <ModernButton first={config.thirdColor}>Bachelor of Science in Computer Science</ModernButton>
+        <h1 className='text-[2rem] md:text-[3rem]'>{home.heading}</h1>
         <div className='pl-5'>
             {home.titles.map((title, index) => (
-                <h1 key={index} className='text-[1rem] md:text-[1.5rem] text-black'>{title}</h1>
+                <h1 key={index} className='text-[1rem] md:text-[1.5rem] text-black'>
+                  <TypeAnimation 
+                    sequence={[
+                      index * 400,
+                      title
+                    ]}
+                    wrapper="span"
+                    speed={85}
+                    repeat={0}
+                    cursor={false}
+                  />
+                </h1>
             ))}
         </div>
         <Buttons />
--- app/static/home.ts
+++ app/static/home.ts
@@ -1,8 +1,8 @@
 export default Object.freeze({
-    heading: "I am Thanh Nha (Will) Phan",
-    titles: ["Full Stack Engineer", "DevOps Engineer", "AI Engineer", "Machine Learning Enthusiast"],
-    intro: "Opportunities are everywhere",
-    des: "Every moment always is a good chance for me to learn and gain experience in anything and everything. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others.",
+    heading: "I am Will Phan",
+    titles: ["Fullstack", "DevOps", "AI/ML"],
+    intro: "Bridging Visionary Innovation with Scalable Engineering",
+    des: "Every moment always is a good chance for me to learn and gain experience in anything and everything. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others. I am dedicated to architecting high-impact solutions by turning complex challenges into elegant digital experiences. I thrive at the intersection of technical excellence and continuous growth, leveraging a meticulous approach to problem-solving and a passion for building scalable, resilient systems that drive innovation.",
     up: "Software Engineering",
     down: "begins here",
 })
