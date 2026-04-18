48c7693026e2f1a5389b81cc8d98fb47e5dd9edb
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-04-18 11:15:28 -0400

    Fix intro component with style and content

--- app/static/home.ts
+++ app/static/home.ts
@@ -3,7 +3,7 @@ export default Object.freeze({
     heading: "I am Will Phan",
     titles: ["Fullstack", "DevOps", "AI/ML"],
     intro: "Bridging Visionary Innovation with Scalable Engineering",
-    des: "Every moment always is a good chance for me to learn and gain experience in anything and everything. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others. I am dedicated to architecting high-impact solutions by turning complex challenges into elegant digital experiences. I thrive at the intersection of technical excellence and continuous growth, leveraging a meticulous approach to problem-solving and a passion for building scalable, resilient systems that drive innovation.",
+    des: "Every moment is always a good chance for me to learn and gain experience. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others. I am dedicated to architecting high-impact solutions by turning complex challenges into elegant digital experiences. I thrive at the intersection of technical excellence and continuous growth, leveraging a meticulous approach to problem-solving and a passion for building scalable, resilient systems that drive innovation.",
     up: "Software Engineering",
     down: "begins here",
 })
