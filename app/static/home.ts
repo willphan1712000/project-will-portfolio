a5f9fe5f7845f8291a6b4f9957069453edaf1d11
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-04-23 15:55:17 -0400

    Change font, font weight, and color

--- app/static/home.ts
+++ app/static/home.ts
@@ -1,6 +1,6 @@
 export default Object.freeze({
     subheading: ["Bachelor of Science in Computer Science", "Software Engineering"],
-    heading: "I am Will Phan",
+    heading: "Will Phan",
     titles: ["Fullstack", "DevOps", "AI/ML"],
     intro: "Bridging Visionary Innovation with Scalable Engineering",
     des: "Every moment is always a good chance for me to learn and gain experience. Significantly, experience can be gained from the way we live, how we observe and solve problems, and the way we treat ourselves and others. I am dedicated to architecting high-impact solutions by turning complex challenges into elegant digital experiences. I thrive at the intersection of technical excellence and continuous growth, leveraging a meticulous approach to problem-solving and a passion for building scalable, resilient systems that drive innovation.",
