70697e04fb3e65511c19a45789fa24f173a9f9f8
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-06-11 23:45:24 -0400

    Modify titles

--- app/static/home.ts
+++ app/static/home.ts
@@ -3,7 +3,7 @@ import getGoogleImage from "../utilities/getGoogleImage";
 export default Object.freeze({
     avatar: getGoogleImage("12z_7SJj3WX5unECGGMjOOOrCe2hP2ERf"),
     heading: "Will Phan",
-    titles: ["Fullstack", "DevOps", "AI/ML"],
+    titles: ["Full-stack Engineering", "AI Engineering", "IT Support Engineering"],
     intro: "I am here to help you achieve your goals in Tech",
     des: "My name is Nha Phan, people call me Will as it is easier to say 😄 I am a transfer student from the University of Technology in Vietnam. Because I wanted to explore who I am and what I am really good at, then I came to the US to attend college here and explore the life here to help better understand myself. After several years of studying and experiencing, I have gained a lot of knowledge and experiences in Tech industry and discovered exactly what I love doing which is building impactful software focusing on Software Engineering, DevOps Engineering, and AI/ML Engineering. You can come across my Portfolio and see what projects I've done, what library I've created, what experience I've been through, and where I've travelled. Then you can understand how those skills can benefit you and your company by solving your company problems. I have a Bachelor's Degree in Computer Science with Math minor and AI concentration. Feel free to reach out to me at willphan1712@gmail.com.",
     up: "Software Engineering",
