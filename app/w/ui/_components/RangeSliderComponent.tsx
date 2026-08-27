bae85dfb958be280545e1ccf912122452ca63438
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-08-26 22:19:51 -0400

    Refactor: update UI components for @willphan1712000/frontend

--- app/w/ui/_components/RangeSliderComponent.tsx
+++ app/w/ui/_components/RangeSliderComponent.tsx
@@ -1,5 +1,4 @@
-'use state'
-
+'use client'
 
 import { useState } from 'react'
 import Code from '../../_components/Code'
@@ -7,33 +6,41 @@ import { RangeSlider } from '@willphan1712000/frontend'
 import { tableList } from './TableOfContents'
 
 const slider = `import { RangeSlider } from '@willphan1712000/frontend';
+import { useState } from 'react';
 
-  const RangerSlider = () => {
-    const [value, setValue] = useState<number>("50")
-    return <RangeSlider
-              min="10"
-              max="400" 
-              onChange={setValue}
-              value={value}
-              color='#673ab7'
-              width='200'
-            />
-  }`
+const RangeSliderExample = () => {
+  const [value, setValue] = useState<string>("50")
+
+  return (
+    <RangeSlider
+      range={{ min: "10", max: "400" }}
+      setValue={setValue}
+      value={value}
+      description="Range Slider Component"
+      styling={{
+        primaryColor: '#673ab7',
+        width: '200'
+      }}
+    />
+  )
+}`
 
 const RangeSliderComponent = () => {
     const [value, setValue] = useState<string>("50")
   return (
     <div>
         <h1 id={tableList[1].id} className='text-2xl'>{tableList[1].table.name}</h1>
-        <Code filename='RangerSlider.tsx' code={slider}/>
+        <Code filename='RangeSlider.tsx' code={slider}/>
         <div className='text-black w-full p-[50px] rounded-xl bg-white flex justify-center'>
             <RangeSlider
-              min="10"
-              max="400" 
-              onChange={setValue}
+              range={{ min: "10", max: "400" }}
+              setValue={(v) => setValue(v ?? '50')}
               value={value}
-              color='#673ab7'
-              width='200'
+              description="Range Slider Component"
+              styling={{
+                primaryColor: '#673ab7',
+                width: '200'
+              }}
             />
         </div>
         <br></br>
--- app/w/ui/_components/SelectComponent.tsx
+++ app/w/ui/_components/SelectComponent.tsx
@@ -6,91 +6,60 @@ import Code from '../../_components/Code';
 import { tableList } from './TableOfContents';
 
 const options = [
-  { label: 'Nodejs', value: 'Nodejs' },
-  { label: 'Prisma', value: 'Prisma' },
-  { label: 'React', value: 'React' },
-  // { label: 'Typescript', value: 'Typescript' },
-  // { label: 'Express', value: 'Express' },
-  // { label: 'Mongodb', value: 'Mongodb' },
-  // { label: 'Graphql', value: 'Graphql' },
-  // { label: 'Docker', value: 'Docker' },
-  // { label: 'Nextjs', value: 'Nextjs' },
-  // { label: 'Tailwindcss', value: 'Tailwindcss' },
-  // { label: 'Redux', value: 'Redux' },
-  // { label: 'Javascript', value: 'Javascript' },
-  // { label: 'Vue', value: 'Vue' },
-  // { label: 'Angular', value: 'Angular' },
-  // { label: 'Jest', value: 'Jest' },
-  // { label: 'Firebase', value: 'Firebase' },
-  // { label: 'Nestjs', value: 'Nestjs' },
-  // { label: 'Vscode', value: 'Vscode' },
-  // { label: 'Sass', value: 'Sass' },
-  // { label: 'Webpack', value: 'Webpack' },
-  // { label: 'Npm', value: 'Npm' },
-  // { label: 'Yarn', value: 'Yarn' },
-  // { label: 'Flask', value: 'Flask' },
-  // { label: 'Django', value: 'Django' },
-  // { label: 'Php', value: 'Php' },
-  // { label: 'Mysql', value: 'Mysql' },
-  // { label: 'Postgresql', value: 'Postgresql' },
-  // { label: 'Python', value: 'Python' },
-  // { label: 'Aws', value: 'Aws' },
-  // { label: 'Azure', value: 'Azure' },
-  // { label: 'Cloudflare', value: 'Cloudflare' },
-  // { label: 'Cypress', value: 'Cypress' },
-  // { label: 'Mocha', value: 'Mocha' },
-  // { label: 'Chai', value: 'Chai' },
-  // { label: 'Html', value: 'Html' },
-  // { label: 'Css', value: 'Css' },
-  // { label: 'Bulma', value: 'Bulma' },
-  // { label: 'Jquery', value: 'Jquery' },
-  // { label: 'Git', value: 'Git' },
-  // { label: 'Github', value: 'Github' },
-  // { label: 'Gitlab', value: 'Gitlab' },
-  // { label: 'Bitbucket', value: 'Bitbucket' },
-  // { label: 'Kubernetes', value: 'Kubernetes' },
-  // { label: 'Vagrant', value: 'Vagrant' },
-  // { label: 'Bash', value: 'Bash' },
-  // { label: 'Zsh', value: 'Zsh' },
-  // { label: 'Markdown', value: 'Markdown' },
-  // { label: 'Json', value: 'Json' },
-  // { label: 'Csv', value: 'Csv' },
+  { label: <span>Nodejs</span>, labelContent: 'Nodejs', value: 'Nodejs' },
+  { label: <span>Prisma</span>, labelContent: 'Prisma', value: 'Prisma' },
+  { label: <span>React</span>, labelContent: 'React', value: 'React' },
+  { label: <span>Typescript</span>, labelContent: 'Typescript', value: 'Typescript' },
+  { label: <span>Express</span>, labelContent: 'Express', value: 'Express' },
+  { label: <span>Mongodb</span>, labelContent: 'Mongodb', value: 'Mongodb' },
+  { label: <span>Graphql</span>, labelContent: 'Graphql', value: 'Graphql' },
+  { label: <span>Docker</span>, labelContent: 'Docker', value: 'Docker' },
+  { label: <span>Nextjs</span>, labelContent: 'Nextjs', value: 'Nextjs' },
 ];
 
 const single = `import { DropdownSelect } from '@willphan1712000/frontend';
+import { useState } from 'react';
 
-  const options = [
-    { label: 'Nodejs', value: 'Nodejs' },
-    { label: 'Prisma', value: 'Prisma' },
-    { label: 'React', value: 'React' },
-  ]
+const options = [
+  { label: <span>Nodejs</span>, labelContent: 'Nodejs', value: 'Nodejs' },
+  { label: <span>Prisma</span>, labelContent: 'Prisma', value: 'Prisma' },
+  { label: <span>React</span>, labelContent: 'React', value: 'React' },
+]
 
-  const DropdownSelect = () => {
-    const [value, setValue] = useState<any[]>('')
-    return <DropdownSelect 
-              onChange={singleChange}
-              value={singleValue}
-              options={options}
-              width='300'
-          />
-  }`
+const DropdownSelectExample = () => {
+  const [value, setValue] = useState<string>('')
+
+  return (
+    <DropdownSelect 
+      setValue={setValue}
+      value={value}
+      options={options}
+      description="Dropdown Select Component"
+    />
+  )
+}`
+
 const multiple = `import { MultiSelect } from '@willphan1712000/frontend';
+import { useState } from 'react';
 
-  const options = [
-    { label: 'Nodejs', value: 'Nodejs' },
-    { label: 'Prisma', value: 'Prisma' },
-    { label: 'React', value: 'React' },
-  ]
+const options = [
+  { label: <span>Nodejs</span>, labelContent: 'Nodejs', value: 'Nodejs' },
+  { label: <span>Prisma</span>, labelContent: 'Prisma', value: 'Prisma' },
+  { label: <span>React</span>, labelContent: 'React', value: 'React' },
+]
 
-  const MultiSelect = () => {
-    const [value, setValue] = useState<any[]>([])
-    return <MultiSelect 
-            onChange={multipleChange}
-            value={multipleValue}
-            options={options}
-            width='300'
-          />
-  }`
+const MultiSelectExample = () => {
+  const [value, setValue] = useState<string[]>([])
+
+  return (
+    <MultiSelect 
+      setValue={setValue}
+      value={value}
+      options={options}
+      description="MultiSelect Component"
+    />
+  )
+}`
 
 const SelectComponent = () => {
     const [singleValue, singleChange] = useState<string>('')
@@ -99,23 +68,26 @@ const SelectComponent = () => {
     <div>
         <h1 id={tableList[0].id} className='text-2xl'>{tableList[0].table.name}</h1>
         <Code filename='DropdownSelect.tsx' code={single} />
-        <div className='bg-white flex items-start justify-center p-4 rounded-md m-5 h-[20rem] overflow-hidden text-black'>
+        <div className='bg-white flex items-start justify-center p-10 rounded-md m-5 h-[20rem] overflow-hidden text-black'>
           <div className='w-[300px]'>
             <DropdownSelect 
-                onChange={singleChange}
+                setValue={(v) => singleChange(v ?? '')}
                 value={singleValue}
                 options={options}
+                description="Dropdown Select Component"
             />
           </div>
         </div>
         <Code filename='MultiSelect.tsx' code={multiple} />
-        <div className='bg-white flex items-start justify-center p-4 rounded-md m-5 h-[20rem] overflow-hidden text-black'>
-          <MultiSelect 
-            onChange={multipleChange}
-            value={multipleValue}
-            options={options}
-            width='300'
-          />
+        <div className='bg-white flex items-start justify-center p-10 rounded-md m-5 h-[20rem] overflow-hidden text-black'>
+          <div className="w-[300px]">
+            <MultiSelect 
+              setValue={(v) => multipleChange(v ?? [])}
+              value={multipleValue}
+              options={options}
+              description="MultiSelect Component"
+            />
+          </div>
         </div>
         <br></br>
     </div>
